4380954facba1a65baf56e2f455918aa2fefd3d8
mallowlabs <mallowlabs@gmail.com>
2022-07-24 09:11:31 +0900

    Fixed: JavaDoc error

--- src/main/java/org/jenkinsci/plugins/warnumbering/WarNumberingBuilder.java
+++ src/main/java/org/jenkinsci/plugins/warnumbering/WarNumberingBuilder.java
@@ -37,7 +37,7 @@ public class WarNumberingBuilder extends Builder {
 
     /**
      * Constructor
-     * 
+     *
      * @param outputType
      *            output type option
      */
@@ -48,7 +48,7 @@ public class WarNumberingBuilder extends Builder {
 
     /**
      * output type option.
-     * 
+     *
      * @return 'hardlink' or 'move'
      */
     public String getOutputType() {
@@ -126,11 +126,6 @@ public class WarNumberingBuilder extends Builder {
     /**
      * Descriptor for {@link WarNumberingBuilder}. Used as a singleton. The
      * class is marked as public so that it can be accessed from views.
-     * 
-     * <p>
-     * See
-     * <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
      */
     @Extension
     // This indicates to Jenkins that this is an implementation of an extension
